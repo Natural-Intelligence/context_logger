1cdad7b2af994e78f06bcf366522894f40a3137d
DavidRonNI <david.ron@naturalint.com>
2017-08-10 18:57:53 +0300

    Change back to Api

--- app/controllers/api/v1/resources/context_loggers_controller.rb
+++ app/controllers/api/v1/resources/context_loggers_controller.rb
@@ -1,14 +1,14 @@
-module API
+module Api
   module V1
     module Resources
       class ContextLoggersController < ::ActionController::Base
         def index
-            if params[:action_id].nil?
-              render json: {error: 'Missing mandatory parameter action_id'}, :status => 400, layout: false
-              return
-            end
-            #TODO: add pagination
-            render json: ::ContextLog.where(action_id: params[:action_id]).all, layout: false, :root => false
+          if params[:action_id].nil?
+            render json: {error: 'Missing mandatory parameter action_id'}, :status => 400, layout: false
+            return
+          end
+          #TODO: add pagination
+          render json: ::ContextLog.where(action_id: params[:action_id]).all, layout: false, :root => false
         end
       end
     end
