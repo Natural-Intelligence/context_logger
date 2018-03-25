6e3cecc19786080c313fc7cd307c7f3906534678
blackcolt <idanmagled@gmail.com>
2018-03-25 13:50:58 +0300

    Revert " - Added nill protection."
    This reverts commit ede031a

--- app/models/context_log.rb
+++ app/models/context_log.rb
@@ -3,8 +3,6 @@ class ContextLog < ActiveRecord::Base
   before_save :trim_message
 
   def trim_message
-    unless self[:message].nil?
-      self[:message] = self[:message][0..20000]
-    end
+    self.attributes[:message] = self.attributes[:message][0..20000]
   end
 end
